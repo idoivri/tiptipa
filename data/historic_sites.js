1ebfdf384a86822e5d090d2efd991287377281be
idoivri <idoivri@gmail.com>
2014-09-15 05:24:56 +0300

    added js

---
+++ data/historic_sites.js
@@ -0,0 +1,79 @@
+stations={
+    {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+           35.003197,
+           32.596897
+          ]
+        },
+        "properties": {
+          "marker-symbol": "commercial",
+          "site-name": "בית הכנסת ובית השוחט בבת שלמה",
+          "גוש": "11345",
+          "חלקה": "16",
+          "כתובת": "רחוב המושבה",
+          "שנת הקמה": "1888-1889",
+          "תאריך תיק תיעוד" : "אוגוסט 2013",
+          "שם עורך 1" : "אדר' טל פרידמן ראודניץ",
+          "שם עורך 2" : "אדר' לילך גינת נוימרק",
+          "שם עורך 3" : "",
+          "קישור לתיק" : "https://www.dropbox.com/s/ppys5ly5hjeklbg/%2B%C2%AC%2B%C3%96%2B%C2%BA_%2B%C2%AC%2B%C3%96%2B%C3%B3%2B%C3%B2%2B%C3%B4_%2B%C3%A6%2B%C3%96%2B%C2%AC_%2B%C3%B6%2B%C2%A2%2B%C3%A1%2B%C3%AD%2B%C2%AC_%2B%C3%A6%2B%C3%A6%2B%C2%AC_%2B%C2%AC%2B%C2%A3%2BP%2B%C3%B6.pdf?dl=0",
+          "thumbnail" : "https://github.com/idoivri/tiptipa/blob/gh-pages/data/windmill.png"
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [   
+          35.224177,
+          31.771555
+          ]
+        },
+        "properties": {
+          "marker-symbol": "commercial",
+          "site-name": "טחנת הרוח במשכנות שאננים",
+          "גוש": "30029",
+          "חלקה": "12",
+          "כתובת": "רחוב היינה",
+          "שנת הקמה": "1857",
+          "תאריך תיק תיעוד" : "אוגוסט 2011",
+          "שם עורך 1" : "ג. קרטס",
+          "שם עורך 2" : "",
+          "שם עורך 3" : "",
+          "קישור לתיק" : "https://www.dropbox.com/s/upr9nov3nv7iqrg/%2B%C2%AC%2B%C3%B9%2B%C3%A1%2B%C2%AC_%2B%C3%B6%2B%C2%BA%2BP%2B%C3%B9_%2B%C3%A6%2BP%2B%C2%AC%2B%C2%A2%2B%C3%A1%2B%C3%B2%2B%C2%AC_%2B%C2%AC%2B%C3%89%2B%C3%A1%2B%C3%A1%2B%C3%96%2B%C2%A5_-_%2B%C3%86%2B%C3%A6%2B%C2%BF%2B%C3%96%2B%C3%89%2B%C2%A3_%2B%C2%BA%2B%C2%BF%2B%C3%BF%2B%C3%AD.pdf?dl=0",
+          "thumbnail" : ""
+        }
+      },
+      {
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [   
+          34.945364,
+          32.519098
+          ]
+        },
+        "properties": {
+          "marker-symbol": "commercial",
+          "site-name": "בתי גבעת הפועל",
+          "גוש": "10207",
+          "חלקה": "7",
+          "כתובת": "רחוב השניים 11",
+          "שנת הקמה": "1929",
+          "תאריך תיק תיעוד" : "ספטמבר 2012",
+          "שם עורך 1" : "אדר' טל פרידמן ראודניץ",
+          "שם עורך 2" : "אדר' לילך גינת נוימרק",
+          "שם עורך 3" : "",
+          "קישור לתיק" : "https://www.dropbox.com/s/u3nkip6ed6pfx3o/%2B%C3%86%2B%C3%A6%2B%C3%B3%2B%C2%AC_%2B%C3%B6%2B%C3%B1%2B%C3%B2%2B%C3%B3%2B%C2%A3_%2B%C3%A6%2B%C3%A1%2B%C3%96%2BP%2B%C3%96%2B%C3%A1%2B%C3%B6_-_%2BP%2B%C3%B3%2B%C2%AC%2B%C3%B6_%2B%C2%AC%2B%C3%96%2BP%2B%C3%B2%2B%C2%BF.pdf?dl=0",
+          "thumbnail" : ""
+        }
+      }
+    ]
+  }
+}
